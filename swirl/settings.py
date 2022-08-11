90b365b02b7bea303632ebf491a89557eb0f6702
mauLovera <mauricioloveraedu@gmail.com>
2022-08-11 08:06:28 -0400

    fix config

--- swirl/settings.py
+++ swirl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import django_heroku 
 import environ 
 env = environ.Env()
@@ -81,8 +82,12 @@ WSGI_APPLICATION = 'swirl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
